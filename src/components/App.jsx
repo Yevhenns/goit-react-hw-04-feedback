c6990feff65bf505780f0deb7dd9db965efbcf0f
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-11-19 20:28:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 
-export class App extends Component {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  useEffect(() => {});
+};
+export class App1 extends Component {
   state = {
     good: 0,
     neutral: 0,
@@ -50,3 +57,56 @@ export class App extends Component {
     );
   }
 }
+
+// import React, { Component } from 'react';
+// import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+// import Notification from './Notification/Notification';
+// import Section from './Section/Section';
+// import Statistics from './Statistics/Statistics';
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   onIncrement = e => {
+//     this.setState(prevState => ({
+//       [e.target.name]: prevState[e.target.name] + 1,
+//     }));
+//   };
+
+//   countTotalFeedback = () => {
+//     return this.state.good + this.state.neutral + this.state.bad;
+//   };
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.ceil((this.state.good * 100) / this.countTotalFeedback());
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <Section title={'Please leave feedback'}>
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.onIncrement}
+//           />
+//         </Section>
+//         <Section title={'Statistics'}>
+//           {this.countTotalFeedback() === 0 ? (
+//             <Notification message={'There is no feedback'} />
+//           ) : (
+//             <Statistics
+//               good={this.state.good}
+//               neutral={this.state.neutral}
+//               bad={this.state.bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage() || 0}
+//             />
+//           )}
+//         </Section>
+//       </>
+//     );
+//   }
+// }
